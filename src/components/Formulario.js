245c0544da70e2ebb8739ad7483278cee76677a3
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-27 16:56:57 -0500

    we create the Form component and created Hook

---
+++ src/components/Formulario.js
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { useMoneda } from '../Hooks/useMoneda';
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66A2FE;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #FFF;
+    transition: background-color .3s ease;
+
+    &:hover{
+        background-color: #326AC0;
+        cursor: pointer;
+    }
+`;
+
+export const Formulario = () => {
+
+    const MONEDAS = [
+        { codigo: 'USD', nombre: 'Dolar de Estado Unidos' },
+        { codigo: 'MXM', nombre: 'Peso Mexicano' },
+        { codigo: 'EUR', nombre: 'Euro' },
+        { codigo: 'GBP', nombre: 'Libra Esterlina' },
+    ]
+
+    // * Utilizar useMoneda
+    const [ moneda, SelectMonedas ] = useMoneda('Elige tu Moneda: ', '',MONEDAS);
+
+    return (
+        <form>
+            <SelectMonedas />
+            
+           <Boton 
+                type="submit"
+                value="Calcular"
+           /> 
+        </form>
+    )
+}
