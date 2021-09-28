0934f8eb489f7088780d1e18cf25a52ae1cce8a7
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-27 23:29:20 -0500

    we add styled components to Cotizacion

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,15 +1,36 @@
+import styled from '@emotion/styled';
 import React from 'react'
 
+const ResultadoDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+
+    span {
+        font-weight: bold;
+    }
+`;
 export const Cotizacion = ({ resultado }) => {
 
     if( Object.keys( resultado ).length === 0) return null;
     return (
-        <div>
-            <p>El precio es de: <span>{ resultado.PRICE }</span></p>
-            <p>Precio más alto del dia es de: <span>{ resultado.HIGHDAY }</span></p>
-            <p>Precio más bajo del dia es de: <span>{ resultado.LOWDAY }</span></p>
-            <p>Variación últimas 24 horas es de: <span>{ resultado.CHANGEPCT24HOUR }</span></p>
-            <p>Última Actualizacion es de: <span>{ resultado.LASTUPDATE }</span></p>
-        </div>
+        <ResultadoDiv>
+            <Precio>El precio es de: <span>{ resultado.PRICE }</span></Precio>
+            <Info>Precio más alto del dia es de: <span>{ resultado.HIGHDAY }</span></Info>
+            <Info>Precio más bajo del dia es de: <span>{ resultado.LOWDAY }</span></Info>
+            <Info>Variación últimas 24 horas es de: <span>{ resultado.CHANGEPCT24HOUR }</span></Info>
+            <Info>Última Actualizacion es de: <span>{ resultado.LASTUPDATE }</span></Info>
+        </ResultadoDiv>
     )
 }
